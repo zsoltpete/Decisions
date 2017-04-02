df5f7f8f1e7d22acedec0c157f8e653a2da87ce8
zsoltpete <pete.zsolt95@gmail.com>
2017-04-02 18:28:58 +0200

    :construction: Project files

--- src/main/java/com/petez/decisions/Controllers/GameViewController.java
+++ src/main/java/com/petez/decisions/Controllers/GameViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.petez.decisions;
+package com.petez.decisions.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
