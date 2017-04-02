623a611670956e525034c7df3d2bb4ccf2b3f82a
zsoltpete <pete.zsolt95@gmail.com>
2017-04-02 19:14:38 +0200

    :sparkles: Start button works

--- src/main/java/com/petez/decisions/Controllers/MenuViewController.java
+++ src/main/java/com/petez/decisions/Controllers/MenuViewController.java
@@ -5,9 +5,17 @@
  */
 package com.petez.decisions.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +24,11 @@ import javafx.fxml.Initializable;
  */
 public class MenuViewController implements Initializable {
 
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button shopButton;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +36,15 @@ public class MenuViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void startGame(ActionEvent event) throws IOException {
+        Stage stage = (Stage) startButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/GameView.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/gameview.css");
+        stage.setScene(scene);
+    }
     
 }
