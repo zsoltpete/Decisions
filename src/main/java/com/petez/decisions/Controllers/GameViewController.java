7ca64ea0fd7f17d11c258e6e7578f35d4c0dd9dd
zsoltpete <pete.zsolt95@gmail.com>
2017-04-02 19:15:03 +0200

    :sparkles: Menu button works

--- src/main/java/com/petez/decisions/Controllers/GameViewController.java
+++ src/main/java/com/petez/decisions/Controllers/GameViewController.java
@@ -5,9 +5,17 @@
  */
 package com.petez.decisions.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +24,9 @@ import javafx.fxml.Initializable;
  */
 public class GameViewController implements Initializable {
 
+    @FXML
+    private Button backButton;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +34,15 @@ public class GameViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void backToMenu(ActionEvent event) throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MenuView.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/gameview.css");
+        stage.setScene(scene);
+    }
     
 }
