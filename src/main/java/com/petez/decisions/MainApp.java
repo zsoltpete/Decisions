23287776a97c45aff326c2705dec6d46f3e1d6c9
zsoltpete <pete.zsolt95@gmail.com>
2017-04-02 18:51:16 +0200

    :construction: Project files

--- src/main/java/com/petez/decisions/MainApp.java
+++ src/main/java/com/petez/decisions/MainApp.java
@@ -12,7 +12,8 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/GameView.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/GameView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MenuView.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/gameview.css");
